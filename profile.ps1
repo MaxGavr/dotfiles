69c0fa8871076e349d9b0a15927d0da27f7243d8
Max Gavrilyuk <max.gavr.gmi@gmail.com>
2019-06-03 19:18:22 +0300

    Add PowerShell profile

---
+++ profile.ps1
@@ -0,0 +1,5 @@
+﻿# run "PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force" to install posh-git
+# run "PowerShellGet\Update-Module posh-git" to update it
+Import-Module posh-git
+
+Set-Alias -Name g -Value git
